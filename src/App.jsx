871526b5a26fb3f0c057fa317c189b287e1d786a
khushi0515 <khushikumariks05@gmail.com>
2025-01-24 10:54:14 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Hero from "./components/Hero";
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"> 
         <Header />
         <Hero />
       </div>
+      
       <ButtonGradient />
     </>
   );
