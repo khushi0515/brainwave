897838f610b3106523cc89d77c7087482434fce8
khushi0515 <khushikumariks05@gmail.com>
2025-01-26 22:55:52 +0530

    new file created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Benefits from "./components/Benefits";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"> 
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
-      
+
       <ButtonGradient />
     </>
   );
---
+++ src/components/Benefits.jsx
@@ -0,0 +1,14 @@
+import Heading from "./Heading";
+import Section from "./Section";
+
+const Benefits = () => {
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading className="" title="Chat Smarter, Not Harder with" />
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;
