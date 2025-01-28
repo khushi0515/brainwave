acf817534d29b637cc0c70a0c0d69706c7393226
khushi0515 <khushikumariks05@gmail.com>
2025-01-28 20:28:54 +0530

    fhg

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,3 +1,4 @@
+import { check } from "../assets";
 import { collabContent } from "../constants";
 import Section from "./Section";
 
@@ -14,6 +15,7 @@ const Collaboration = () => {
             <li className="mb-3 py-3" key={item.id}>
               <div className="flex items-center">
                 <img src={check} width={24} height={24} />
+                <h6>{item.title}</h6>
               </div>
             </li>
            ))}
