512cfdd5eae2d7cd13a9e313db363ee9d1913a42
khushi0515 <khushikumariks05@gmail.com>
2025-01-31 14:50:49 +0530

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
