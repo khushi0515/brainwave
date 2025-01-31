57fbd28b6f704255e4c7d7694d3bd0b24adbd6db
khushi0515 <khushikumariks05@gmail.com>
2025-01-31 15:07:17 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 import { brainwave } from "../assets";
@@ -7,6 +6,8 @@ import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
+
 const Header = () => {
   const pathname = useLocation();
   const [openNavigation, setOpenNavigation] = useState(false);
@@ -30,7 +31,7 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full  z-50  border-b  border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
@@ -38,6 +39,7 @@ const Header = () => {
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
+
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
@@ -61,6 +63,7 @@ const Header = () => {
               </a>
             ))}
           </div>
+
           <HamburgerMenu />
         </nav>
 
@@ -70,7 +73,9 @@ const Header = () => {
         >
           New account
         </a>
-        <Button className="hidden lg:flex href=#login">Sign in</Button>
+        <Button className="hidden lg:flex" href="#login">
+          Sign in
+        </Button>
 
         <Button
           className="ml-auto lg:hidden"
